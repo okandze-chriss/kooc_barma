e074cd55706dbb67bef534abc4964e37aaefd107
okandze-chriss <okandzechrissgit@gmail.com>
2024-06-12 20:02:12 +0200

    Controle des buttons

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,86 +1,77 @@
 import 'package:flutter/material.dart';
+import '../../utils/constants.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isClicked = false;
+  String resultValue = "";
+
+  List<String> images = [];
+
+  List<String> result = [];
+
+  List<String> totalLetter = [];
+
+  @override
+  void initState() {
+    resultValue = "boisson";
+    images = [
+      'images/boisson/boisson1.jpg',
+      'images/boisson/boisson2.webp',
+      'images/boisson/boisson3.jpg',
+      'images/boisson/boisson4.webp'
+    ];
+    result = initEmptyList(resultValue.length);
+    totalLetter = koocClavier(resultValue);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // List<String> images = [
-    //   'images/boisson/boisson1.jpg',
-    //   'images/boisson/boisson2.webp',
-    //   'images/boisson/boisson3.jpg',
-    //   'images/boisson/boisson4.webp'
-    // ];
-    List<String> images = [
-      'images/bruit/bruit.jpeg',
-      'images/bruit/bruit2.jpg',
-      'images/bruit/bruit3.jpg',
-      'images/bruit/bruit4.webp'
-    ];
-
-    List<String> aphabet = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z"
-    ];
-
-    List<String> result = ['B', 'R', 'U', 'I', 'T'];
-
-    BorderRadius border = BorderRadius.circular(6);
-    int totalLetter = 7;
-    int totalLetterValue = 5;
-
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 1, 3, 41),
-      body: Container(
-        color: const Color(0xFF242c43),
-        padding: const EdgeInsets.all(20.0),
+      backgroundColor: KoocUtils.bgColor,
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            GridView.count(
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              children: List.generate(
-                  images.length,
-                  (index) => Card(
-                        shadowColor: Colors.white60,
-                        color: const Color(0xFF161a2a),
-                        borderOnForeground: true,
-                        child: Container(
-                          width: 150.0,
-                          height: 150.0,
-                          padding: const EdgeInsets.all(4.0),
-                          margin: const EdgeInsets.all(6.0),
-                          decoration: BoxDecoration(
-                              borderRadius: border,
-                              image: DecorationImage(
-                                  image: AssetImage(images[index]),
-                                  fit: BoxFit.fill)),
-                        ),
-                      )),
+            Container(
+              color: KoocUtils.borderColor.withOpacity(0.3),
+              height: 60.0,
+              width: MediaQuery.of(context).size.width,
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            Container(
+              padding: const EdgeInsets.all(12.0),
+              width: (MediaQuery.of(context).size.width * 0.8).toDouble(),
+              child: GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                children: List.generate(
+                    images.length,
+                    (index) => Card(
+                          shadowColor: Colors.white60,
+                          color: KoocUtils.fieldGgColor,
+                          borderOnForeground: true,
+                          child: Container(
+                            width: 150.0,
+                            height: 150.0,
+                            padding: const EdgeInsets.all(4.0),
+                            margin: const EdgeInsets.all(6.0),
+                            decoration: BoxDecoration(
+                                borderRadius: KoocUtils.borderRadius,
+                                image: DecorationImage(
+                                    image: AssetImage(images[index]),
+                                    fit: BoxFit.fill)),
+                          ),
+                        )),
+              ),
             ),
             const SizedBox(
               height: 20.0,
@@ -90,19 +81,21 @@ class HomePage extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  for (int index = 0; index < totalLetterValue; index++)
+                  for (int index = 0; index < resultValue.length; index++)
                     Container(
                       alignment: Alignment.center,
                       margin: const EdgeInsets.all(2.0),
-                      width: 40,
-                      height: 40,
+                      width:
+                          (MediaQuery.of(context).size.width / 10).toDouble(),
+                      height:
+                          (MediaQuery.of(context).size.width / 10).toDouble(),
                       decoration: BoxDecoration(
-                          borderRadius: border,
-                          color: const Color(0xFF161a2a),
+                          borderRadius: KoocUtils.borderRadius,
+                          color: KoocUtils.fieldGgColor,
                           border: Border.all(
-                              color: const Color(0xff374e86), width: 2)),
+                              color: KoocUtils.borderColor, width: 2)),
                       child: Text(
-                        result[index],
+                        result.isNotEmpty ? result[index].toUpperCase() : '',
                         style: const TextStyle(
                           color: Colors.white,
                           fontSize: 18,
@@ -119,70 +112,101 @@ class HomePage extends StatelessWidget {
             Container(
               alignment: Alignment.center,
               margin: EdgeInsets.zero,
-              child: Column(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (int index = 0; index < totalLetter; index++)
-                        Container(
-                          margin: const EdgeInsets.all(6.0),
-                          alignment: Alignment.center,
-                          width: 35,
-                          height: 35,
-                          decoration: BoxDecoration(
-                              borderRadius: border,
-                              color: index == totalLetter - 1
-                                  ? const Color(0xffb0d800)
-                                  : const Color(0xffdedede),
-                              border:
-                                  Border.all(color: const Color(0xff374e86))),
-                          child: index == totalLetter - 1
-                              ? const Icon(
-                                  Icons.abc,
-                                  color: Colors.white,
-                                )
-                              : Text(
-                                  aphabet[index].toUpperCase(),
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w700,
-                                      color: Color(0xff151519)),
-                                ),
-                        ),
-                    ],
+                  Flexible(
+                    flex: 3,
+                    child: SizedBox(
+                      width:
+                          (MediaQuery.of(context).size.width * 0.7).toDouble(),
+                      child: GridView.count(
+                        crossAxisCount: 6,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: List.generate(
+                            totalLetter.length,
+                            (index) => GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      result[index] = totalLetter[index];
+                                      isClicked = true;
+                                    });
+
+                                    if (index == result.length - 1) {
+                                      dialogBuilder(
+                                        context,
+                                        () {
+                                          setState(() {
+                                            resultValue = "bruit";
+                                            result = initEmptyList(
+                                                resultValue.length);
+                                            totalLetter =
+                                                koocClavier(resultValue);
+                                            images = [
+                                              'images/bruit/bruit.jpeg',
+                                              'images/bruit/bruit2.jpg',
+                                              'images/bruit/bruit3.jpg',
+                                              'images/bruit/bruit4.webp'
+                                            ];
+                                          });
+                                          Navigator.of(context).pop();
+                                        },
+                                      );
+                                    }
+                                  },
+                                  child: Container(
+                                      margin: const EdgeInsets.only(
+                                          top: 2.0,
+                                          right: 2.0,
+                                          left: 2.0,
+                                          bottom: 4.0),
+                                      alignment: Alignment.center,
+                                      width: 40,
+                                      height: 40,
+                                      decoration: BoxDecoration(
+                                          borderRadius: KoocUtils.borderRadius,
+                                          color: KoocUtils.btnGrayColor,
+                                          border: Border.all(
+                                              color: KoocUtils.borderColor)),
+                                      child: Text(
+                                        totalLetter[index].toUpperCase(),
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w700,
+                                            color: KoocUtils.textColor),
+                                      )),
+                                )),
+                      ),
+                    ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (int index = 0; index < totalLetter; index++)
-                        Container(
-                          margin: const EdgeInsets.all(6.0),
-                          width: 35,
-                          height: 35,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              borderRadius: border,
-                              color: index == totalLetter - 1
-                                  ? const Color(0xffb0d800)
-                                  : const Color(0xffdedede),
-                              border: Border.all(
-                                  color: const Color(0xff374e86),
-                                  style: BorderStyle.solid)),
-                          child: index == totalLetter - 1
-                              ? const Icon(
-                                  Icons.delete_forever_rounded,
-                                  color: Colors.white,
-                                )
-                              : Text(
-                                  aphabet[index + 7].toUpperCase(),
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w700,
-                                      color: Color(0xff151519)),
-                                ),
-                        ),
-                    ],
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.zero,
+                      child: Column(
+                        children: [
+                          for (int index = 0; index < 2; index++)
+                            Container(
+                              margin: const EdgeInsets.only(
+                                  top: 2.0, right: 2.0, left: 2.0, bottom: 4.0),
+                              alignment: Alignment.center,
+                              width: 40,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                  borderRadius: KoocUtils.borderRadius,
+                                  color:
+                                      KoocUtils.btnGreenColor.withOpacity(0.5),
+                                  border:
+                                      Border.all(color: KoocUtils.borderColor)),
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
---
+++ lib/utils/constants.dart
@@ -0,0 +1,119 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class KoocUtils {
+  static BorderRadius borderRadius = BorderRadius.circular(6);
+  static const Color bgColor = Color(0xFF242c43);
+  static const Color fieldGgColor = Color(0xFF161a2a);
+  static const Color btnGreenColor = Color(0xffb0d800);
+  static const Color btnGrayColor = Color(0xffdedede);
+  static const Color borderColor = Color(0xff374e86);
+  static const Color textColor = Color(0xff151519);
+  static const Color btnClickColor = Color(0xff353d51);
+
+  static const List<String> aphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+}
+
+List<String> koocClavier(String mot) {
+  List<String> letters = [];
+
+  for (var i = 0; i < mot.length; i++) {
+    letters.add(mot[i]);
+  }
+
+  for (var i = mot.length; i < 12; i++) {
+    letters.add(getRandomString());
+  }
+
+  return letters;
+}
+
+String getRandomString() {
+  const String characters = 'abcdefghijklmnopqrstuvwxyz';
+  var r = Random();
+  return String.fromCharCodes(List.generate(
+      1, (_) => characters.codeUnitAt(r.nextInt(characters.length))));
+}
+
+List<String> initEmptyList(int length) {
+  List<String> values = [];
+  for (var i = 0; i < length; i++) {
+    values.add('');
+  }
+  return values;
+}
+
+Future<void> dialogBuilder(BuildContext context, Function()? onPressed) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+        title: const Text('Basic dialog title'),
+        content: const SizedBox(
+          height: double.maxFinite,
+          child: Column(
+            children: [
+              Text(
+                'A dialog is a type of modal window that\n'
+                'appears in front of app content to\n'
+                'provide critical information, or prompt\n'
+                'for a decision to be made.',
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+                onPressed: onPressed,
+                style: ButtonStyle(
+                    padding: const MaterialStatePropertyAll<EdgeInsetsGeometry>(
+                        EdgeInsets.all(20.0)),
+                    shape: const MaterialStatePropertyAll<OutlinedBorder>(
+                        LinearBorder.none),
+                    backgroundColor: MaterialStatePropertyAll(
+                        KoocUtils.btnGreenColor.withOpacity(0.5)),
+                    textStyle: const MaterialStatePropertyAll(
+                        TextStyle(color: KoocUtils.textColor))),
+                child: const Text(
+                  'Continuer',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                )),
+          )
+        ],
+      );
+    },
+  );
+}
